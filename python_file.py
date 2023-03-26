408d8e51f8ce7ce3573bc9201690d8a110a206ae
JitDemo <128039000+JitDemo@users.noreply.github.com>
2023-03-26 09:49:50 +0300

    Update python_file.py

--- python_file.py
+++ python_file.py
@@ -3,7 +3,4 @@ import subprocess
 subprocess.Popen(["ls"], shell=True)
 subprocess.Popen(["ls"], shell=True)
 subprocess.Popen(["ls"], shell=True)
-subprocess.Popen(["ls"], shell=True)
-subprocess.Popen(["ls"], shell=True)
-subprocess.Popen(["ls"], shell=True)
 subprocess.Popen(["ls -l"], shell=False)
