bfe021ac3c09731d3a897b48bfd7a305f451b7fd
JonathanRosenboim <101969613+JonathanRosenboim@users.noreply.github.com>
2024-04-03 20:15:31 +0300

    Update dynamodb.tf

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,15 +1,16 @@
-resource "aws_s3_bucket" "example_bucket" {
-  bucket = "example-bucket"
-}
-
-resource "aws_s3_bucket_cors_configuration" "example_cors" {
-  bucket = aws_s3_bucket.example_bucket.id
-
-  cors_rule {
-    allowed_headers = ["*"]
-    allowed_methods = ["GET", "POST", "PUT", "DELETE"]
-    allowed_origins = ["*"]
-    expose_headers  = ["ETag"]
-    max_age_seconds = 3000
-  }
-}
+AWSTemplateFormatVersion: "2010-09-11"
+Description: Sample CloudFormation template for DynamoDB with AWS-Owned CMK
+Resources:
+  DynamoDBOnDemandTable4:
+    Type: "AWS::DynamoDB::Table"
+    Properties:
+      TableName: "dynamodb-kms-2"
+      AttributeDefinitions:
+        - AttributeName: pk
+          AttributeType: S
+      KeySchema:
+        - AttributeName: pk
+          KeyType: HASH
+      BillingMode: PAY_PER_REQUEST
+      SSESpecification:
+        SSEType: "KMS"
