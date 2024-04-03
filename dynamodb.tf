90323fd1ee45656d3ff205325c3756a5fe0d53bd
JitDemo <128039000+JitDemo@users.noreply.github.com>
2024-04-03 16:11:06 +0300

    Update dynamodb.tf

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,12 +1,32 @@
-resource "aws_dynamodb_table" "example" {
-  name           = "example-table"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 5
-  write_capacity = 5
-  hash_key       = "id"
+resource "aws_lambda_function" "example" {
+  filename         = "lambda_function_payload.zip"
+  function_name    = "example-lambda"
+  role             = aws_iam_role.lambda_exec.arn
+  handler          = "index.handler"
+  runtime          = "nodejs14.x"
 
-  attribute {
-    name = "id"
-    type = "S"
+  environment {
+    variables = {
+      SECRET_KEY = "mysupersecretkey"
+    }
   }
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "serverless_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
 }
